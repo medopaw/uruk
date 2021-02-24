7bd76e14e126b31e9ada3b92a10955b117f630ce
Med0paW <medopaw@gmail.com>
2021-02-24 17:47:07 +0800

    MOD: Exit on any error

--- install.sh
+++ install.sh
@@ -98,6 +98,11 @@ install_one() {
     echo Installing "$1"...
     add_execution_permission "$script_path"
     source_script "$script_path"
+    exit_code=$?
+    if [ $exit_code -ne 0 ]
+    then
+        exit $exit_code
+    fi
 }
 
 current_dir=$PWD
