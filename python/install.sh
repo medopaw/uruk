06699775f4d7dae9cf2b5bb76cf5e634b055a455
Med0paW <medopaw@gmail.com>
2021-02-26 16:18:19 +0800

    MOD: Update pyenv before installing python

--- python/install.sh
+++ python/install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 install_if_needed pyenv
+install_if_needed brew
+brew update && brew upgrade pyenv
 install_if_needed fzf
 latest_python_version=`pyenv install -l | fzf`
 pyenv install "$latest_python_version"
