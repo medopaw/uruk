0f35052d889d5cce386fec8348a82e7f59d38684
Med0paW <medopaw@gmail.com>
2021-02-26 16:47:59 +0800

    FIX: Python leading spaces of fzf result

--- python/install.sh
+++ python/install.sh
@@ -6,7 +6,7 @@ brew update && brew upgrade pyenv
 install_if_needed fzf
 echo "Please choose python version to install. Press ENTER to continue:"
 read
-python_version=`pyenv install -l | fzf`
+python_version=`pyenv install -l | fzf | sed -e 's/^\s*//'`
 echo "Installing python $python_version now..."
 pyenv install "$python_version"
 pyenv global "$python_version"
