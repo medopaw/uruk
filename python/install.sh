e2ee63855596be266428372bf27a62839f74b201
Med0paW <medopaw@gmail.com>
2021-02-26 16:53:16 +0800

    FIX: Remove all spaces from fzf result

--- python/install.sh
+++ python/install.sh
@@ -6,7 +6,7 @@ brew update && brew upgrade pyenv
 install_if_needed fzf
 echo "Please choose python version to install. Press ENTER to continue:"
 read
-python_version=`pyenv install -l | fzf | sed -e 's/^\s*//'`
+python_version=`pyenv install -l | fzf | tr -d ' '`
 echo "Installing python $python_version now..."
 pyenv install "$python_version"
 pyenv global "$python_version"
--- ruby/install.sh
+++ ruby/install.sh
@@ -4,7 +4,7 @@ install_if_needed rbenv
 install_if_needed fzf
 echo "Please choose ruby version to install. Press ENTER to continue:"
 read
-ruby_version=`rbenv install -l | fzf | sed -e 's/^\s*//'`
+ruby_version=`rbenv install -l | fzf | tr -d ' '`
 echo "Installing ruby $ruby_version now..."
 rbenv install "$ruby_version"
 rbenv global "$ruby_version"
