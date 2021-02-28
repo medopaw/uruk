470175c67893d6280d17475ff07e587b3445a672
Med0paW <medopaw@gmail.com>
2021-03-01 01:00:15 +0800

    MOD: Test pyenv/rbenv in profile

--- pyenv.sh
+++ pyenv.sh
@@ -2,5 +2,5 @@
 
 install_if_needed brew
 brew install pyenv
-append_to_profile_if_needed "eval \"\$(pyenv init -)\""
+append_to_profile_if_needed "if command -v pyenv 1>/dev/null 2>&1; then eval \"\$(pyenv init -)\"; fi"
 source_profile
--- rbenv.sh
+++ rbenv.sh
@@ -2,5 +2,5 @@
 
 install_if_needed brew
 brew install rbenv
-append_to_profile_if_needed "eval \"\$(rbenv init -)\""
+append_to_profile_if_needed "if command -v rbenv 1>/dev/null 2>&1; then eval \"\$(rbenv init -)\"; fi"
 source_profile
