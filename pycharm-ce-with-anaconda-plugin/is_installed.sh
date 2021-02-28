e7d58b9f10ebfdd8bb1729b079f1400c09bf5053
Med0paW <medopaw@gmail.com>
2021-03-01 03:23:41 +0800

    FIX: Typo

--- pycharm-ce-with-anaconda-plugin/is_installed.sh
+++ pycharm-ce-with-anaconda-plugin/is_installed.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-exists_in_application_folder "PyCharm CE with Anaconda plugn"
-is_installed_by_brew pycharm-ce-with-anaconda-plugin
+exists_in_application_folder "PyCharm CE with Anaconda plugin"
 return
