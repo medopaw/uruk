17f50595587b050753665238640dd7de2c7a63a9
Med0paW <medopaw@gmail.com>
2021-03-04 13:21:36 +0800

    ADD: brew source option

--- targets/brew/install.sh
+++ targets/brew/install.sh
@@ -9,9 +9,10 @@ if [ ! $(git config --global user.email) ]; then
     git config --global user.email "$email"
 fi
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# If failed, run local pre-downloaded script
-if ! is_installed brew; then
+echo "Do you want to install brew using USTC mirrors? It will accelerate downloads in China."
+echo "Enter yes to use USTC mirrors, anything else to use official source:"
+read user_input
+if [ "$user_input" = 'yes' ]; then
     # First clone brew-cask from faster source in China to save time
     cask_dir=/usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask
     mkdir -p "$cask_dir"
@@ -19,4 +20,6 @@ if ! is_installed brew; then
     /bin/bash targets/brew/brew.sh
     append_to_profile_if_needed "export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles"
     source_profile
+else
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
